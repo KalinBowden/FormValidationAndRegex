af0b36a4305a91ec9d2afd4ea669f75cb571ecef
Kalin Bowden <KalinBowden@gmail.com>
2017-10-06 18:09:50 -0500

    added mor val

--- fv.js
+++ fv.js
@@ -15,10 +15,13 @@ function onFormLoad()
 function validateForm()
 {
     //
+    var warnBox = document.getElementById("warning");
     var email = document.getElementById("emailId");
     var name = document.getElementById("nameId");
     var phone = document.getElementById("phoneId");
-    var radio = document.getElementById("radioMain");
+    var radio0 = document.getElementById("radio1");
+    var radio1 = document.getElementById("radio2");
+    var radio2 = document.getElementById("radio3");
 
 
     //
@@ -26,11 +29,13 @@ function validateForm()
     {
         email.style.background = warn;
         email.style.border = "1px solid red";
+        warnBox.style.display = "block";
     }
     else
     {
         email.style.background = "";
         email.style.border = "";
+        warnBox.style.display = "none";
     }
 
 
@@ -39,11 +44,13 @@ function validateForm()
     {
         name.style.background = warn;
         name.style.border = "1px solid red";
+        warnBox.style.display = "block";
     }
     else
     {
         name.style.background = "";
         name.style.border = "";
+        warnBox.style.display = "none";
     }
 
 
@@ -52,24 +59,34 @@ function validateForm()
     {
         phone.style.background = warn;
         phone.style.border = "1px solid red";
+        warnBox.style.display = "block";
+    }
+    else if ()
+    {
+        
     }
     else
     {
         phone.style.background = "";
         phone.style.border = "";
+        warnBox.style.display = "none";
     }
 
 
      //
-     if (radio.value === "")
+     if(radio0.checked === false && radio1.checked === false && radio2.checked === false)
      {
-         radio.style.background = warn;
-         radio.style.border = "1px solid red";
+        radio0.style.outline = "1px solid red";
+        radio1.style.outline = "1px solid red";;
+        radio2.style.outline = "1px solid red";
+        warnBox.style.display = "block";
      }
      else
      {
-         radio.style.background = "";
-         radio.style.border = "";
+        radio0.style.outline = "";
+        radio1.style.outline = "";
+        radio2.style.outline = "";
+        warnBox.style.display = "none";
      }
 }
 
